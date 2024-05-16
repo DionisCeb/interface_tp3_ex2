503dc9ae00fbc125cc1353260e76c499669df3e4
Dionis <e2396498@cmaisonneuve.qc.ca>
2024-05-16 18:14:30 -0400

    example2

---
+++ assets/js/donnees/questions.js
@@ -0,0 +1,42 @@
+const questions = [
+    {
+        question: "What is my prefered color?",
+        answers: [
+            {text: "Red", correct: false},
+            {text: "Green", correct: false},
+            {text: "White", correct: true},
+            {text: "Black", correct: false},
+        ]
+    },
+    {
+        question: "What is my prefered car?",
+        answers: [
+            {text: "BMW", correct: false},
+            {text: "Audi", correct: false},
+            {text: "Suzuki", correct: false},
+            {text: "Mercedes", correct: true},
+        ]
+    },
+    {
+        question: "What is my prefered brand?",
+        answers: [
+            {text: "Nike", correct: false},
+            {text: "Puma", correct: true},
+            {text: "Adidas", correct: false},
+            {text: "ZARA", correct: false},
+        ]
+    },
+    {
+        question: "What is my prefered browser?",
+        answers: [
+            {text: "Google", correct: true},
+            {text: "Firefox", correct: false},
+            {text: "Safari", correct: false},
+            {text: "Explorer", correct: false},
+        ]
+    },
+
+];
+
+
+export default questions;
